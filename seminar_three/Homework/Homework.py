e0537e4c0fd0fec6a3656a5dbbb5211d3c0f57b8
Valery_Khramov <dr.hramoff2013@yandex.ru>
2023-12-05 00:04:00 +0400

    added: Homework three version 1.0

---
+++ seminar_three/Homework/Homework.py
@@ -0,0 +1,71 @@
+# Размерность поля
+board_size = 3
+# Игровое поле
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def draw_board():
+    ''' Выводим игровое поле'''
+
+    print('_' * 4 * board_size)
+    for i in range(board_size):
+        print((' ' * 3 + '|') * board_size)
+        print('', board[i * board_size], '|', board[1 + i * board_size], '|', board[2 + i * board_size], '|')
+        print(('_' * 3 + '|') * board_size)
+
+
+def game_step(index, char) -> bool:
+    '''Выполняем ход'''
+    if index > 9 or index < 1 or board[index - 1] in ('X', 'O'):
+        return False
+    board[index - 1] = char
+    return True
+
+
+def check_win() -> bool:
+    '''Проверяем комбинацию на победу'''
+    win = False
+    win_combination = ((0, 1, 2), (3, 4, 5), (6, 7, 8),
+                       (0, 3, 6), (1, 4, 7), (2, 5, 8),
+                       (0, 4, 8), (2, 4, 6)
+                       )
+    for pos in win_combination:
+        if board[pos[0]] == board[pos[1]] and board[pos[1]] == board[pos[2]]:
+            win = board[pos[0]]
+    return win
+
+
+def start_game():
+    # Текущий игрок
+    current_player = 'X'
+    # Номер шага
+    step = 1
+    draw_board()
+    while (step < 10) and (check_win() == False):
+        # лучше ввести проверку на ввод
+        index = input("Ходит игрок: " + current_player + ". Введите номер поля (0 - выход): ")
+
+        if index == '0':
+            break
+
+        if game_step(int(index), current_player):
+            print("Удачный ход")
+
+            if current_player == 'X':
+                current_player = 'O'
+            else:
+                current_player = 'X'
+
+            draw_board()
+            step += 1
+        else:
+            print("Неверный номер! Повторите!")
+
+    if step == 10:
+        print("Игра окончена! Ничья!")
+    else:
+        print("Выиграл: "+check_win())
+
+if __name__ == '__main__':
+    print("Добро пожаловать в крестики-нолики!")
+    start_game()
